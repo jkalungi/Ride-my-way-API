ccd726bd5d6fae8441f328464f203102dad98a9f
jkalungi <shannyjolly@yahoo.co.uk>
2018-06-25 02:04:48 -0700

    adding APIs

--- app/api/offers/__init__.py
+++ app/api/offers/__init__.py
@@ -2,9 +2,9 @@ from flask import Blueprint
 from flask_restful import Api
 from .views import AddRequest, EditRequest, GetRequests, GetOneRequest
 
-request = Blueprint('requests', __name__)
-maintainance_api = Api(request)
-maintainance_api.add_resource(AddRequest, '/api/v1/users/requests')
-maintainance_api.add_resource(GetRequests, '/api/v1/users/requests')
-maintainance_api.add_resource(GetOneRequest, '/api/v1/users/requests/<request_id>')
-maintainance_api.add_resource(EditRequest, '/api/v1/users/requests/<request_id>')
+request = Blueprint('rides', __name__)
+my app_api = Api(ride)
+my app_api.add_resource(AddRequest, '/api/v1/users/rides')
+my app_api.add_resource(GetRequests, '/api/v1/users/rides')
+my app_api.add_resource(GetOneRequest, '/api/v1/users/rides/<request_id>')
+my app_api.add_resource(EditRequest, '/api/v1/users/rides/<request_id>')
